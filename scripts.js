327156b6050ac58eabc85b7db7d2bf0b5b22fe44
Alan P Santos <alan.ps_tec@outlook.com>
2023-05-11 08:18:10 -0300

    Atualizando

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 const horas = document.getElementById('Horas');
+const minutos = document.getElementById('Minutos');
+const segundos = document.getElementById('Segundos');
+
+const relogio = setInterval(function() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    horas.textContent = hr;
+
+})
